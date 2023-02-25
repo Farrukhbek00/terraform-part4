bf320ae24bf439c09567c2157dc62dfcf4fc56e8
Farrukhbek00 <farruhzokirov00@gmail.com>
2023-02-25 17:13:15 +0500

    remove locking for now

--- src/data.tf
+++
@@ -1,8 +0,0 @@
-data "terraform_remote_state" "networking" {
-    backend = "s3"
-    config = {
-        bucket = "farruh-tfstate-part2"
-        key    = "networking.tf"
-        region = "eu-west-1"
-    }
-}
--- src/provider.tf
+++ src/provider.tf
@@ -7,7 +7,6 @@ terraform {
     bucket         = "farruh-tfstate-part2"
     key            = "application.tf"
     region         = "eu-west-1"
-    dynamodb_table = "part2locktable"
   }
 
   required_version = "= 1.3.9"
