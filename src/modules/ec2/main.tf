d883421923969f6e56734a99c5ae592292ebe782
Farrukhbek00 <farruhzokirov00@gmail.com>
2023-02-26 17:02:39 +0500

    add new tfstate file

--- src/modules/ec2/main.tf
+++ src/modules/ec2/main.tf
@@ -11,24 +11,24 @@ resource "aws_instance" "Public-ec2" {
 
     subnet_id              = "${var.subnet_public_id}"
     vpc_security_group_ids = ["${var.sg_public_id}"]
-    # key_name               = "${aws_key_pair.key_pair.id}"
+    key_name               = "${aws_key_pair.key_pair.id}"
 
     tags = {
         Name: "Public-ec2-${count.index + 1}"
     }
 }
 
-# resource "tls_private_key" "key" {
-#   algorithm = "RSA"
-# }
+resource "tls_private_key" "key" {
+  algorithm = "RSA"
+}
 
-# resource "local_file" "private_key" {
-#   filename          = "key.pem"
-#   sensitive_content = tls_private_key.key.private_key_pem
-#   file_permission   = "0400"
-# }
+resource "local_file" "private_key" {
+  filename          = "key.pem"
+  sensitive_content = tls_private_key.key.private_key_pem
+  file_permission   = "0400"
+}
 
-# resource "aws_key_pair" "key_pair" {
-#   key_name   = "key"
-#   public_key = tls_private_key.key.public_key_openssh
-# }
+resource "aws_key_pair" "key_pair" {
+  key_name   = "key"
+  public_key = tls_private_key.key.public_key_openssh
+}
